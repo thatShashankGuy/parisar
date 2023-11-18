fe0d6151ed4be5d48733eaf6a2c02f538fd83490
thatshashankguy <shahankforworkshekhar@gmail.com>
2023-11-18 11:34:57 +0530

    sentence struct changes

--- web/src/Constants/DataObjects.ts
+++ web/src/Constants/DataObjects.ts
@@ -38,12 +38,12 @@ export const AboutMe = {
     heading : "A bit about me ",
     P1 : `In my 6 years of career in IT ,I have been fortunate enough to wear many hats and take on various roles and projects. Currently working with Winjit Technologies as a Senior Software engineer (Products) .
     Working in various tech spaces including web application, network applications, Cloud Automation DevOps, RPA automation, production support, business intelligent etc. gave me a wide perspective and appreciation
-    for various business and technological solutions we provide for them.`,
+    for various business and techincal solutions.`,
     P2 : `I have a bechelors degree in Electronic and Communications Engineering which have provided me with skill of deep analysis,problem solving and thinking out of the box.`,
     P3 : `My keen interest in computer science goes beyond my day job and i like fiddling around new paradigms , languages and abstract concepts which fills most of my unstructured git repositories.`,
     P4 : `I am an avid reader and prefer hardcopies over ebooks. I love gaming and between that and my job i spend most of time staring at different screens`
 }
 
 export const landing = {
-    message : "Hello and Welcome. I am Shashank and I appreciate you visitng my website."
+    message : "Hello, I am Shashank, and welcome to my very elaborate business card."
 }
