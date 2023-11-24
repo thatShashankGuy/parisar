25e47ce2f47b7056259e415dc2a0a2b37eb7dcb8
thatshashankguy <shahankforworkshekhar@gmail.com>
2023-11-24 09:39:02 +0530

    some changes

--- web/src/Components/Landing/Landing.tsx
+++ web/src/Components/Landing/Landing.tsx
@@ -21,7 +21,10 @@ const Landing = () => {
       <Typography variant="h5" component="h1" gutterBottom sx={{
           fontSize: { xs: '1.5rem', sm: '2rem', md: '2.5rem' },
           wordBreak: 'break-word' 
-      }}>  {welcomeMessage}
+      }}> Hello, I am <span style={{
+        fontFamily: "monospace",
+        fontWeight: 'bold',
+      }}>Shashank</span>, and welcome to my very elaborate business card.
       <span><FontAwesomeIcon icon={faMeteor} /></span>
     </Typography>
         </Box>
