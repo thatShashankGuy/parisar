ad93e94f2e21f2c47325a50246a4ef831cb00b79
thatshashankguy <shahankforworkshekhar@gmail.com>
2023-12-29 00:52:46 +0530

    issues with SSG

--- kendra/svelte.config.js
+++ kendra/svelte.config.js
@@ -15,11 +15,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: undefined, // index.html  for spa
-			pages: 'build',
-			assets: 'build',
-			precompress: false,
-			strict: true
+			fallback: 'index.html' // for spa
+			// pages: 'build',
+			// assets: 'build',
+			// precompress: false,
+			// strict: true
 		})
 	}
 };
